5b1d5b494d74548a3f00974d7fb30732b78f77e8
Muhammad Hikmat Mustofa <144418504+hikmatkaka8@users.noreply.github.com>
2023-11-01 11:39:23 +0700

    Update Capaian.java

--- src/model/Capaian.java
+++ src/model/Capaian.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Capaian {
+    private ListArray<Challange> challanges;
+    
     // TODO meresume dan menyimpulkan semua challange yang dikerjakan oleh user ataupun admin
+    public void summarize() {}
+
+    //TODO 
+    public void getPoints() {}
 }
